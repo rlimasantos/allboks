c67a20b4819d383f48bcef25e64e296f006bb1fc
Ricardo Santos <101815120+rlimasantos@users.noreply.github.com>
2026-02-11 11:29:47 -0300

    Update linha 21 server.js

--- src/server.js
+++ src/server.js
@@ -18,5 +18,6 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
+  console.log("Alterando algo")
   console.log("ACesse essa API em http://localhost:8000")
 })
